b1bd64ed16f7683f29d6df56fed55527e11ecae8
MomchilGalabov <m.p.galabov@gmail.com>
2023-04-14 16:44:14 +0300

    added passwordEncoder and modelMapper instances in main method

--- src/main/java/com/example/soundcloudfinalprojectittalentss15/SoundCloudFinalProjectItTalentsS15Application.java
+++ src/main/java/com/example/soundcloudfinalprojectittalentss15/SoundCloudFinalProjectItTalentsS15Application.java
@@ -1,13 +1,26 @@
 package com.example.soundcloudfinalprojectittalentss15;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class SoundCloudFinalProjectItTalentsS15Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SoundCloudFinalProjectItTalentsS15Application.class, args);
+	}
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
