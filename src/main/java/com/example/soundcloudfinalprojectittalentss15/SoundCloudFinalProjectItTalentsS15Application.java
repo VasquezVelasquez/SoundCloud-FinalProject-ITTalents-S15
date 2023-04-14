8573f6274484bd857cbfb21205612b5fe5b2aab7
VasquezVelasquez <vasil.sariev12@gmail.com>
2023-04-14 15:49:16 +0300

    upload project files

---
+++ src/main/java/com/example/soundcloudfinalprojectittalentss15/SoundCloudFinalProjectItTalentsS15Application.java
@@ -0,0 +1,13 @@
+package com.example.soundcloudfinalprojectittalentss15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundCloudFinalProjectItTalentsS15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoundCloudFinalProjectItTalentsS15Application.class, args);
+	}
+
+}
