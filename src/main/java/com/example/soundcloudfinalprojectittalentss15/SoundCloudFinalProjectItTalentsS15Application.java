e5306e0b05b1e99e484e3a52e851fe53f8080974
MomchilGalabov <m.p.galabov@gmail.com>
2023-04-18 00:00:15 +0300

    added mailSender

--- src/main/java/com/example/soundcloudfinalprojectittalentss15/SoundCloudFinalProjectItTalentsS15Application.java
+++ src/main/java/com/example/soundcloudfinalprojectittalentss15/SoundCloudFinalProjectItTalentsS15Application.java
@@ -1,11 +1,15 @@
 package com.example.soundcloudfinalprojectittalentss15;
 
 
+import com.example.soundcloudfinalprojectittalentss15.services.EmailSenderService;
 import jakarta.persistence.EntityManager;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
@@ -25,7 +29,15 @@ public class SoundCloudFinalProjectItTalentsS15Application {
 		return new ModelMapper();
 	}
 
+	@Autowired
+	private EmailSenderService senderService;
 
+	@EventListener(ApplicationReadyEvent.class)
+	public void sendMail() {
+		senderService.sendMail("MomchilGalabov@outlook.com",
+				"Subject",
+				"Email body");
+	}
 
 
 }
